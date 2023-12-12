a55f488552f60da3378ce0820396f31506e0110b
Jupiter Dogherty <alinagud14@yandex.ru>
2023-12-12 21:37:34 +0400

    add parsing groups and teachers

---
+++ parsing.py
@@ -0,0 +1,47 @@
+import json
+import codecs
+import requests
+from bs4 import BeautifulSoup
+
+
+def groupParser():
+    groups = []
+    result = {"groups": []}
+
+    for i in range(1, 6):
+        url = "https://ssau.ru/rasp/faculty/492430598?course=" + str(i)
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            group_list = soup.select(".group-catalog__groups > a")
+
+            for group in group_list:
+                group_name = group.text
+                group_link = "/rasp" + group['href'][group['href'].find('?'):]
+
+                groups.append({"name": group_name, "link": group_link})
+                result["groups"] = groups
+
+                with open("group.json", "w") as json_file:
+                    json.dump(result, json_file)
+
+def teacherParser():
+    teachers = []
+    result = {"teachers": []}
+    for i in range(1, 122):
+        url = "https://ssau.ru/staff?page=" + str(i)
+        response = requests.get(url)
+        teachers.append(response.text)
+        if i == 121:
+            for teacher in teachers:
+                soup = BeautifulSoup(teacher, 'html.parser')
+                teachers_list = soup.select(".list-group-item > a")
+                for t in teachers_list:
+                    staffId = ''.join(filter(str.isdigit, t.get("href")))
+                    result["teachers"].append({ "name": t.text, "link": f"/rasp?staffId={staffId}" })
+            with codecs.open("teacher.json", "w", "utf-8") as stream:
+                stream.write(json.dumps(result, ensure_ascii=False))
+
+
+groupParser()
+teacherParser()
